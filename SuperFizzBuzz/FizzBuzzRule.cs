ec3c79491dc3fc37c6ddd0ae643a136d0d6be28d
Josh Hufford <josh@hufford.io>
2021-04-23 18:29:09 -0400

    Implement FizzBuzzRule

--- SuperFizzBuzz/FizzBuzzRule.cs
+++ SuperFizzBuzz/FizzBuzzRule.cs
@@ -1,4 +1,18 @@
+using System;
 namespace SuperFizzBuzz
 {
-    public record FizzBuzzRule(int Divisor, string Token);
+    public class FizzBuzzRule
+    {
+        public int Divisor { get; }
+        public string Token { get; }
+
+        public FizzBuzzRule(int divisor, string token)
+        {
+            if (divisor <= 0)
+                throw new ArgumentException("Divisor must be positive.", nameof(divisor));
+                
+            Divisor = divisor;
+            Token = token ?? throw new ArgumentNullException(nameof(token));
+        }
+    }
 }
